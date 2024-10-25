04ebcc10007b9e70fa6a68156d6777d9b40959e3
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-10-25 10:12:08 +0200

    exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const blogStyles = {
+    border: "1px solid black",
+    margin: "1rem 0",
+    padding: "0.5rem",
+  };
+
+  console.log("data", blog);
+
+  return (
+    <div style={blogStyles}>
+      <p>
+        {blog.title} by {blog.author}
+      </p>
+      {showDetails && (
+        <>
+          <p>{blog.url}</p>
+          <p>likes: {blog.likes}</p>
+          <p>{blog.user.username}</p>
+        </>
+      )}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "show"}
+      </button>
+    </div>
+  );
+};
+
+export default Blog;
