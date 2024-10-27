88a4c2a9490a1eda71f0ddb82a94a4a759ca86b9
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2024-10-27 11:16:56 +0100

    exercise 5.14.

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -20,3 +20,25 @@ test("renders content", () => {
   expect(div).not.toHaveTextContent("Test url");
   expect(div).not.toHaveTextContent("likes: 0");
 });
+
+test("after clicking the button, children are displayed", async () => {
+  const blog = {
+    author: "Test author",
+    title: "Test title",
+    url: "Test url",
+    likes: 0,
+  };
+
+  const { container } = render(<Blog blog={blog} />);
+
+  const div = container.querySelector(".blog");
+  expect(div).not.toHaveTextContent("Test url");
+  expect(div).not.toHaveTextContent("likes: 0");
+
+  const user = userEvent.setup();
+  const button = screen.getByText("show");
+  await user.click(button);
+
+  expect(div).toHaveTextContent("Test url");
+  expect(div).toHaveTextContent("likes: 0");
+});
